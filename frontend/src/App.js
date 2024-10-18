0875a2b766bb8f782bdc8423452936f24a8c7453
tchr7902 <trevorchristensen3405@gmail.com>
2024-10-18 09:51:28 -0600

    working on frontend integration

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TicketPage from "./pages/TicketPage";
+
+function App() {
+    return (
+        <div className="App">
+            <TicketPage />
+        </div>
+    );
+}
+
+export default App;
