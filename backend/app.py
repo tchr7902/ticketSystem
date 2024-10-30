e6337cc10a524b9a2fbcbcfa7df70b48ae50b3af
tchr7902 <trevorchristensen3405@gmail.com>
2024-10-30 13:00:47 -0600

    trying to fix refresh issue

--- backend/app.py
+++ backend/app.py
@@ -46,19 +46,6 @@ def close_db(exception):
     if db is not None:
         db.close()
 
-# Serve static files and the React app
-@app.route('/')
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
-
-@app.errorhandler(404)
-def not_found(e):
-    return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/<path:path>')
-def static_proxy(path):
-    return send_from_directory(app.static_folder, path)
-
 # Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
