112e733f33f9d60c8e51e3ead13d8f9a84869780
tchr7902 <trevorchristensen3405@gmail.com>
2024-11-11 13:47:20 -0700

    updating static file serving for serverside

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, g
+from flask import Flask, request, jsonify, g, send_from_directory
 from flask_cors import CORS
 from config.db_config import connect_to_db
 from routes.ticket_routes import tickets_bp
@@ -70,7 +70,7 @@ def close_db(exception):
 
 @app.route('/<path:path>')
 def catch_all(path):
-    return app.send_static_file('index.html')
+    return send_from_directory('frontend/build', 'index.html')
 
 # Run the Flask app
 if __name__ == '__main__':
