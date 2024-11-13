0ff28a7685fe6a2660ef4b5dcf66bbe68282550c
tchr7902 <trevorchristensen3405@gmail.com>
2024-11-13 16:01:01 -0700

    adding logout function for any unauthorized requests

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -17,4 +17,16 @@ axiosInstance.interceptors.request.use(
     }
 );
 
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            sessionStorage.removeItem('token');
+            sessionStorage.removeItem('tokenExpiration');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default axiosInstance;
