565c156f99f4c93826f9802c5adc8266646914e6
tchr7902 <trevorchristensen3405@gmail.com>
2024-11-18 14:11:44 -0700

    updating cors rules

--- backend/app.py
+++ backend/app.py
@@ -15,14 +15,7 @@ load_dotenv()
 # Initialize Flask app
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 
-CORS(app, supports_credentials=True, origins=[
-    "https://ticketsystem-1.onrender.com",
-    "https://gemtickets.org",
-    "https://gemtickets.org/",
-    "http://localhost:3000",
-    "http://localhost:5000",
-    "http://127.0.0.1:5000"
-])
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
 
 # Configure the JWT secret key
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_KEY')
