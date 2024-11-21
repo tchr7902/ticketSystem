f185faba9dd19987b93f3f2c8b86862fa0b4ca10
tchr7902 <trevorchristensen3405@gmail.com>
2024-11-21 15:31:51 -0700

    got ticket creation with chats completely functional. updating messages for registration

--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -75,7 +75,7 @@ def register():
             message_text = (
                 f"🔔 *Hello {first_name}!*\n\n"
                 f"Thank you for registering with our IT support system! 🎉\n\n"
-                f"You can visit *gemtickets.org* or type */createTicket* in the Good Earth IT chat to create tickets.\n\n"
+                f"You can create, view, update, and delete tickets anytime at *gemtickets.org*. To submit tickets via chats, please create a new Direct Message with 'Ticket System'.\n\n"
                 f"Once we receive your ticket, we will get to it as soon as possible. You'll receive updates on your tickets right here in this chat.\n\n"
                 f"If you have any questions or need further assistance, feel free to reach out to the IT team directly.\n\n"
                 f"Thank you for being part of the team! 😊"
